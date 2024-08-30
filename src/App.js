d150acb0aa3108f05151c79d139921d3db25f7bc
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-08-30 20:56:41 +0300

    carate react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { homeView } from './views/homeView';
+
+function App() {
+  return (
+    <div className="App">
+      homeView
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/homeView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const homeView = () => {
+  return (
+    <div>
+        <h1>Home View</h1>
+    </div>
+  )
+}
