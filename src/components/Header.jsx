61e1073fd0eb141a65607bf9f185af459aec5d95
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-09-01 15:46:12 +0300

    create custom component footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
 import '../styles/Header.css'
+
+import React, { useState, useEffect, useRef } from 'react';
+
 export const Header = () => {
+  
+
   return (
     <header>
       <div className='main_div'>
@@ -9,7 +13,13 @@ export const Header = () => {
         <h1 className='title'>Музей Університету митної справи та фінансів</h1>
       <ul className='categories'>
             <li className='category'>Головна</li>
-            <li className='category'>Про музей</li>
+            <li className='category'>
+              Про музей &#9662;
+              <ul className='sub_categories'>
+                <li className='sub_category'>Історія створення музею</li>
+                <li className='sub_category'>Наші експозиції</li>
+              </ul>
+            </li>
             <li className='category'>Голерея</li>
             <li className='category'>Відвідувачу</li>
             <li className='category'>Контакти</li>
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import '../styles/HomeView.css'
 import { Footer } from '../components/Footer'
 
 export const HomeView = () => {
   return (
-    <div>
+    <div className='main-div'>
       <Header/>
-        <div>
-          <h1></h1>
+        <div className='all-posters'>
+          <h1>Posters DIV</h1>
         </div>
-        <h1>Home View</h1>
+        <div className='all-news'>
+          <h1>News DIV</h1>
+        </div>
+        <div className='all-posters'>
+          <h1>Posters DIV</h1>
+        </div>
+        <div className='all-news'>
+          <h1>News DIV</h1>
+        </div>
+        <div className='all-posters'>
+          <h1>Posters DIV</h1>
+        </div>
+        <div className='all-news'>
+          <h1>News DIV</h1>
+        </div>
+        <div className='all-posters'>
+          <h1>Posters DIV</h1>
+        </div>
+        <div className='all-news'>
+          <h1>News DIV</h1>
+        </div>
+        <div className='all-posters'>
+          <h1>Posters DIV</h1>
+        </div>
+        <div className='all-news'>
+          <h1>News DIV</h1>
+        </div>
 
-        {/* <Footer/> */}
+        <Footer/>
     </div>
   )
 }
