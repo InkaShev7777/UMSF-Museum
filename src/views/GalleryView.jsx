da3750fee691af3bca811ae0f43fd958b4b5f4fe
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-09-17 15:13:51 +0300

    add image carusele for gallery view

--- src/views/GalleryView.jsx
+++ src/views/GalleryView.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { Footer } from '../components/Footer';
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export const GalleryView = () => {
   return (
     <div>
         <Header/>
         <h1>Gallery with image of museum</h1>
+
+        <Carousel autoPlay infiniteLoop showArrows={false} showStatus={false}>
+            <div>
+                <img src='resources/img/gallery-1.jpeg'></img>
+            </div>
+
+            <div>
+                <img src='resources/img/gallery-2.jpeg'></img>
+            </div>
+
+            <div>
+                <img src='resources/img/gallery-4.jpeg'></img>
+            </div>
+        </Carousel>
+
+        <Footer/>
     </div>
   )
 }
